b81d6f6e2efe164ffb177d1965e51a36416211d6
Brandon Gorson <Bgorson@catalytic.com>
2020-11-14 09:10:51 -0400

    Converted App to Functional Component

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
@@ -6,7 +6,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/Home';
 import About from './screens/About';
 import PickerStackScreen from './routes/PickStack';
-import { Footer } from './components/Footer';
 import Header from './components/Header';
 import { FontAwesome } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
@@ -15,97 +14,90 @@ import WelcomeSplash from './screens/WelcomeSplash';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import getTheme from './native-base-theme/components';
 import material from './native-base-theme/variables/material';
+// import {
+//   GoogleSignin,
+//   GoogleSigninButton,
+//   statusCodes,
+// } from 'react-native-google-signin';
 
 import { navigationRef } from './routes/rootNavigate';
 // RN Version > 0.63
 import { LogBox } from 'react-native';
-import { View, StyleProvider } from 'native-base';
+import { StyleProvider } from 'native-base';
 
 LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
 
 const Tab = createBottomTabNavigator();
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isReady: false,
-      isLoggedIn: false,
-    };
-  }
+export default function App() {
+  const [loggedIn, setloggedIn] = useState(false);
+  const [userInfo, setuserInfo] = useState([]);
+  const [isReady, setIsReady] = useState(false);
 
-  async componentDidMount() {
-    await Font.loadAsync({
+  React.useEffect(() => {
+    Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       Nunito: require('./assets/fonts/Nunito-Regular.ttf'),
       ...Ionicons.font,
     });
-    this.setState({ isReady: true });
+    setIsReady(true);
+  }, []);
+
+  if (!isReady) {
+    return <AppLoading />;
   }
 
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
-
-    return !this.state.isLoggedIn ? (
-      <WelcomeSplash
-        loggedIn={() => this.setState({ isLoggedIn: true })}
-      ></WelcomeSplash>
-    ) : (
-      <>
-        <StyleProvider style={getTheme(material)}>
-          <Header />
-        </StyleProvider>
-        <NavigationContainer ref={navigationRef}>
-          <Tab.Navigator
-            initialRouteName="Home"
-            tabBarOptions={{
-              labelStyle: { fontSize: 15 },
-              activeTintColor: '#e91e63',
+  return !loggedIn ? (
+    <WelcomeSplash loggedIn={() => setloggedIn(true)}></WelcomeSplash>
+  ) : (
+    <>
+      <StyleProvider style={getTheme(material)}>
+        <Header />
+      </StyleProvider>
+      <NavigationContainer ref={navigationRef}>
+        <Tab.Navigator
+          initialRouteName="Home"
+          tabBarOptions={{
+            labelStyle: { fontSize: 15 },
+            activeTintColor: '#e91e63',
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarLabel: 'Home',
+              tabBarIcon: () => (
+                <AntDesign name="home" size={24} color="black" />
+              ),
             }}
-          >
-            <Tab.Screen
-              name="Home"
-              component={Home}
-              options={{
-                tabBarLabel: 'Home',
-                tabBarIcon: () => (
-                  <AntDesign name="home" size={24} color="black" />
-                ),
-              }}
-            />
-            <Tab.Screen
-              options={{
-                tabBarLabel: 'Picker',
-                tabBarIcon: () => (
-                  <MaterialCommunityIcons
-                    name="gesture-swipe-right"
-                    size={24}
-                    color="black"
-                  />
-                ),
-              }}
-              name="Picker"
-              component={PickerStackScreen}
-            />
-            <Tab.Screen
-              name="About"
-              component={About}
-              options={{
-                tabBarLabel: 'About',
-                tabBarIcon: () => (
-                  <FontAwesome
-                    name="question-circle-o"
-                    size={24}
-                    color="black"
-                  />
-                ),
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </>
-    );
-  }
+          />
+          <Tab.Screen
+            options={{
+              tabBarLabel: 'Picker',
+              tabBarIcon: () => (
+                <MaterialCommunityIcons
+                  name="gesture-swipe-right"
+                  size={24}
+                  color="black"
+                />
+              ),
+            }}
+            name="Picker"
+            component={PickerStackScreen}
+          />
+          <Tab.Screen
+            name="About"
+            component={About}
+            options={{
+              tabBarLabel: 'About',
+              tabBarIcon: () => (
+                <FontAwesome name="question-circle-o" size={24} color="black" />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
